bbe91507eaba72d5a6d7d8ca48e3e557ee20a61c
Jennifer Rodrigues <jennifer.farias.rodrigues@gmail.com>
2020-08-27 21:16:18 -0300

    Primeiro commit-projeto criado

---
+++ src/test/java/com/jenniferrodrigues/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jenniferrodrigues.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
